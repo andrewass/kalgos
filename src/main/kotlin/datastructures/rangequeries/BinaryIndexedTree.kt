dbe51d3eb970b648f9ab26644b0283d99f6acac9
andrewass <a.wassum@gmail.com>
2019-07-08 19:02:57 +0200

    Added unit tests for Binary Indexed Tree

--- src/main/kotlin/datastructures/rangequeries/BinaryIndexedTree.kt
+++ src/main/kotlin/datastructures/rangequeries/BinaryIndexedTree.kt
@@ -1,16 +1,25 @@
 package datastructures.rangequeries
 
+import java.lang.IllegalArgumentException
+
 /**
+ * An implementation of a Binary Indexed Tree / Fenwick Tree. Used to
+ * find the sum of a range between two indices. The internal array is 1-indexed
  *
+ * @param capacity holds the capacity of the tree
  */
 class BinaryIndexedTree(private val capacity: Int) {
 
     private val tree = LongArray(capacity + 1)
 
     /**
+     * Finds the sum of the range from 1 to a given index inclusive
      *
+     * @param index representing the upper bound of the range
+     * @return the sum of the range between 1 and index inclusive
      */
     fun getSum(index: Int): Long {
+        validateInput(index)
         var indexCopy = index
         var sum = 0L
         while (indexCopy >= 1) {
@@ -21,13 +30,23 @@ class BinaryIndexedTree(private val capacity: Int) {
     }
 
     /**
+     * Adds a given value to element at index, and updates up to log N succeeding elements
      *
+     * @param index representing the element to have its value updated
+     * @param value representing the value to be updated at the index. May be both positive and negative
      */
     fun addValue(index: Int, value: Long) {
+        validateInput(index)
         var indexCopy = index
         while (indexCopy <= capacity) {
             tree[indexCopy] += value
             indexCopy += indexCopy and -indexCopy
+        }
+    }
+
+    private fun validateInput(index: Int) {
+        if (index < 1 || index > capacity) {
+            throw IllegalArgumentException("Index must be within 1 and tree capacity inclusive. Actual index : $index")
         }
     }
 }
--- src/test/kotlin/datastructures/rangequeries/BinaryIndexedTreeTest.kt
+++ src/test/kotlin/datastructures/rangequeries/BinaryIndexedTreeTest.kt
@@ -1,13 +1,70 @@
 package datastructures.rangequeries
 
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
+import kotlin.test.assertEquals
 
 
 class BinaryIndexedTreeTest {
 
+    private lateinit var binaryIndexedTree: BinaryIndexedTree
+    private val capacity = 10
+
     @Test
-    fun shouldFindCorrectRangeSum(){
-        assertTrue(true)
+    fun shouldFindCorrectRangeSumsWithoutAnyUpdates() {
+        val listOfLongs = fillList()
+        binaryIndexedTree = BinaryIndexedTree(capacity)
+        for (i in listOfLongs.indices) {
+            binaryIndexedTree.addValue(i + 1, listOfLongs[i])
+        }
+        val rangeSumOfLastElement = binaryIndexedTree.getSum(10) - binaryIndexedTree.getSum(9)
+        val rangeSumFrom4thTo9thElement = binaryIndexedTree.getSum(9) - binaryIndexedTree.getSum(3)
+
+        assertEquals(rangeSumOfLastElement, 10)
+        assertEquals(rangeSumFrom4thTo9thElement, 121)
+        assertEquals(binaryIndexedTree.getSum(capacity), 194)
+        assertEquals(binaryIndexedTree.getSum(1), 3)
     }
+
+    @Test
+    fun shouldFindCorrectRangeSumsWithoutUpdates() {
+        val listOfLongs = fillList()
+        binaryIndexedTree = BinaryIndexedTree(capacity)
+        for (i in listOfLongs.indices) {
+            binaryIndexedTree.addValue(i + 1, listOfLongs[i])
+        }
+        binaryIndexedTree.addValue(2, -15)
+        binaryIndexedTree.addValue(8, 10)
+        assertEquals(binaryIndexedTree.getSum(capacity), 189)
+    }
+
+    @Test
+    fun shouldFindCorrectRangeSumWhenSomeCellsHoldsDefaultValue() {
+        binaryIndexedTree = BinaryIndexedTree(capacity)
+        binaryIndexedTree.addValue(2, 15)
+        binaryIndexedTree.addValue(8, 10)
+        assertEquals(binaryIndexedTree.getSum(capacity), 25)
+        assertEquals(binaryIndexedTree.getSum(1), 0)
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenIndexIsLessThanOne() {
+        binaryIndexedTree = BinaryIndexedTree(capacity)
+        assertThrows(IllegalArgumentException::class.java) {
+            binaryIndexedTree.addValue(0, 10)
+        }
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenIndexIsAboveCapacity() {
+        binaryIndexedTree = BinaryIndexedTree(capacity)
+        assertThrows(IllegalArgumentException::class.java) {
+            binaryIndexedTree.addValue(11, 10)
+        }
+    }
+
+    private fun fillList() = listOf<Long>(3, 55, 5, 21, 3, 6, 74, 8, 9, 10)
+
 }
