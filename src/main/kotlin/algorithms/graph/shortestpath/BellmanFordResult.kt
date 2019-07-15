01290977df3f6f5079b42a87ee5dad487a477d89
andreaswassum <a.wassum@gmail.com>
2019-07-15 22:26:20 +0100

    Added Bellman-Ford algorithm

---
+++ src/main/kotlin/algorithms/graph/shortestpath/BellmanFordResult.kt
@@ -0,0 +1,4 @@
+package algorithms.graph.shortestpath
+
+class BellmanFordResult {
+}
---
+++ src/test/kotlin/algorithms/graph/shortestpath/BellmanFordKtTest.kt
@@ -0,0 +1,5 @@
+package algorithms.graph.shortestpath
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BellmanFordKtTest
