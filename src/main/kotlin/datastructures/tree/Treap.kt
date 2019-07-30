63d9ec1fa8dd37d50c038c91596f4ab5f7e60a8c
Andreas Wassum <a.wassum@gmail.com>
2019-07-30 22:09:34 +0100

    Updated Treap Operations

--- src/main/kotlin/datastructures/tree/Treap.kt
+++ src/main/kotlin/datastructures/tree/Treap.kt
@@ -1,31 +1,76 @@
 package datastructures.tree
 
+import java.util.*
+
 class Treap {
 
-    var root : TreapNode? = null
+    var root: TreapNode? = null
 
+    /**
+     *
+     */
     fun insert(node: TreapNode) {
-        insert(root, node)
+        var currentNode = root
+        var parent: TreapNode? = null
+        while (currentNode != null && currentNode.priority > node.priority) {
+            parent = currentNode
+            currentNode = if (node.value < currentNode.value) {
+                currentNode.leftChild
+            } else {
+                currentNode.rightChild
+            }
+        }
+        var splitResult = split(currentNode, node.value)
+        when {
+            parent == null -> root = node
+            node.value < parent.value -> parent.leftChild = node
+            else -> parent.rightChild = node
+        }
+        node.leftChild = splitResult.left
+        node.rightChild = splitResult.right
     }
 
-    private fun insert(curr : TreapNode?, newNode : TreapNode){
-
+    /**
+     * Exposed function for deleting a node from the Treap
+     */
+    fun delete(value: Int) {
+        delete(root, value, null)
     }
+
+    /**
+     * Private recursive helper function for deleting a node from the Treap
+     */
+    private fun delete(node: TreapNode?, value: Int, parent: TreapNode?) {
+        when {
+            node == null -> return
+            node.value > value -> delete(node.leftChild, value, node)
+            node.value < value -> delete(node.rightChild, value, node)
+            else -> {
+                val mergedTree = merge(node.leftChild, node.rightChild)
+                when {
+                    parent == null -> root = mergedTree
+                    node == parent.leftChild -> parent.leftChild = mergedTree
+                    else -> parent.rightChild = mergedTree
+                }
+            }
+        }
+    }
+
 
     /**
      * Splits the tree rooted at node, into left and right tree
      */
-    private fun split(treap: TreapNode?, limit: Int): SplitWrapper {
+    private fun split(treap: TreapNode?, splitValue: Int): SplitWrapper {
 
         return if (treap == null) {
             SplitWrapper(null, null)
         } else {
-            if (treap.value < limit) {
-                val res = split(treap.rightChild, limit)
+            if (treap.value < splitValue) {
+                val res = split(treap.rightChild, splitValue)
                 treap.rightChild = res.left
                 SplitWrapper(treap, res.right)
             } else {
-                val res = split(treap.leftChild, limit)
+                val res = split(treap.leftChild, splitValue)
                 treap.leftChild = res.right
                 SplitWrapper(res.left, treap)
             }
@@ -49,6 +94,31 @@ class Treap {
                     right
                 }
             }
+        }
+    }
+
+    /**
+     * Collects a list with [TreapNode] in ascending order
+     *
+     * @return a list of nodes in ascending order
+     */
+    fun inOrderList(): List<TreapNode> {
+        val inOrderList = LinkedList<TreapNode>()
+        fillInOrderList(root, inOrderList)
+        return inOrderList
+    }
+
+    /**
+     * Helper function for collecting the in-order list of [TreapNode]
+     *
+     * @param node current node
+     * @return a linked list storing the nodes
+     */
+    private fun fillInOrderList(node: TreapNode?, inOrderList: LinkedList<TreapNode>) {
+        if (node != null) {
+            fillInOrderList(node.leftChild, inOrderList)
+            inOrderList.addLast(node)
+            fillInOrderList(node.rightChild, inOrderList)
         }
     }
 
--- src/test/kotlin/datastructures/tree/TreapTest.kt
+++ src/test/kotlin/datastructures/tree/TreapTest.kt
@@ -1,16 +1,56 @@
 package datastructures.tree
 
 import implementations.TreapNodeImpl
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class TreapTest {
 
+    lateinit var treap : Treap
+
+    @BeforeEach
+    fun init(){
+        treap = createTreap()
+    }
+
     @Test
-    fun test1() {
+    fun shouldBeABinarySearchTree(){
+        val inOrderList = treap.inOrderList()
+        assertTrue(isInOrder(inOrderList))
+        assertEquals(50, inOrderList.size)
+    }
+
+    @Test
+    fun shouldBeInOrderAfterDeleteOperations(){
+        treap.delete(5)
+        treap.delete(23)
+        treap.delete(11)
+        treap.delete(33)
+        treap.delete(3)
+
+        val inOrderList = treap.inOrderList()
+        assertTrue(isInOrder(inOrderList))
+        assertEquals(45, inOrderList.size)
+    }
+
+    private fun createTreap() : Treap {
         val treap = Treap()
-        for(i in 1..20){
+        for (i in 1..50) {
             treap.insert(TreapNodeImpl(i))
         }
+        return treap
+    }
 
+    private fun isInOrder(list: List<TreapNode>): Boolean {
+        var prevValue : Int? = null
+        for(node in list){
+            when {
+                prevValue != null && node.value < prevValue -> return false
+                else -> prevValue = node.value
+            }
+        }
+        return true
     }
 }
