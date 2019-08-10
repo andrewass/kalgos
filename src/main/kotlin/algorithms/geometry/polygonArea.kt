ac8cfca4109a4d4916cc3e7a677762a3a1940211
Andreas Wassum <a.wassum@gmail.com>
2019-08-11 00:51:41 +0100

    Updated polygon area

--- src/main/kotlin/algorithms/geometry/polygonArea.kt
+++ src/main/kotlin/algorithms/geometry/polygonArea.kt
@@ -1,10 +1,18 @@
 package algorithms.geometry
 
-
-fun polygonArea(points : List<Point>) : Double {
+/**
+ * Calculates the area of a polygon from a list of points.
+ * The points must be ordered, either clockwise or counter-clockwise.
+ * The first and last node of the list must be the same.
+ * If ordered clockwise, the area will be a negative value, else positive.
+ *
+ * @param points List of points of the polygon
+ * @return the area of the polygon
+ */
+fun polygonArea(points: List<Point>): Double {
     var area = 0.00
-    for(i in 0..points.size-2){
-        area += (points[i].x * points[i+1].y - points[i+1].x * points[i].y)
+    for (i in 0..points.size - 2) {
+        area += (points[i].x * points[i + 1].y - points[i + 1].x * points[i].y)
     }
     return area / 2
 }
